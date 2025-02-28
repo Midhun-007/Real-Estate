1fd8c10511c71d2bf0f07f19a17da2822d0e36e6
Midhun.S <midhun1012@gmail.com>
2025-02-28 17:10:02 +0530

    Toast is added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Header from './components/Header'
 import Navbar from './components/Navbar'
 import Projects from './components/Projects'
 
+
 function App() {
 
 
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { ToastContainer, toast } from 'react-toastify';
 
 function Contact() {
     const [result, setResult] = React.useState("");
+    
 
     const onSubmit = async (event) => {
       event.preventDefault();
@@ -20,6 +22,7 @@ function Contact() {
       if (data.success) {
         setResult("Form Submitted Successfully");
         event.target.reset();
+        toast("Your message is recieved");
       } else {
         console.log("Error", data);
         setResult(data.message);
@@ -52,6 +55,8 @@ function Contact() {
             </div>
             <button type='submit'  className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 ">Send Message</button>
             <span>{result}</span>
+            
+            <ToastContainer></ToastContainer>
         </form>
     </div>
   )
